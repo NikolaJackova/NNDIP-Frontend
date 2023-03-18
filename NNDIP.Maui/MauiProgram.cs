2d3c112a0c59a29a9d9e41e619e14469df8c53ad
NikolaJackova <nikol.jac@seznam.cz>
2023-03-18 16:05:50 +0100

    android settings

--- NNDIP.Maui/MauiProgram.cs
+++ NNDIP.Maui/MauiProgram.cs
@@ -16,7 +16,7 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
         var builder = MauiApp.CreateBuilder();
-		builder
+        builder
             .UseMauiApp<App>()
             //.ConfigureSyncfusionCore()
             .UseMauiCommunityToolkit()

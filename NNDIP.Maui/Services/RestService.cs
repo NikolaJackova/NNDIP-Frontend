534941618ea8896a1f7fd793001b39db5c68ec2e
NikolaJackova <nikol.jac@seznam.cz>
2023-03-20 10:08:06 +0100

    remote connection

--- NNDIP.Maui/Services/RestService.cs
+++ NNDIP.Maui/Services/RestService.cs
@@ -11,8 +11,9 @@ namespace NNDIP.Maui.Services
     {
         //TODO uri put somewhere else
         //public static string URI { get; set; } = "http://10.0.2.2:8081";
-        public static string URI { get; set; } = "http://192.168.5.30:8081";
-
+        //public static string URI { get; set; } = "http://192.168.5.30:8081";
+        public static string URI { get; set; } = "http://195.181.223.203:8083";
+        
         private static IApiClient _API = CreateApiClient();
         public static IApiClient API { get => _API; }
 
